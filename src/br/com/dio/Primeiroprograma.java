134495245828588dab8719aa51601dd3bdff628e
RoniQA <ronierisoncosta@gmail.com>
2022-03-16 14:15:30 -0300

    First commit intelij

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+    Gato gato = new Gato();
+
+    public Gato getGato() {
+
+        System.out.println(gato);
+
+        return null;
+    }
+    /*public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello world!" + (a+b));*/
+}
+
+
